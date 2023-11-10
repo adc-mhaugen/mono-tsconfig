9178d903efa350f584c8fd58341d80a31d9bcc3f
Mark Haugen <mhaugen@alarm.com>
2023-11-10 10:07:11 -0500

    Convert tsconfig to use project references

--- apps/app-one/app/controllers/application.ts
+++ apps/app-one/app/controllers/application.ts
@@ -1,8 +1,8 @@
 import Controller from '@ember/controller';
-// import { getRandom } from '@add/one/utils/random';
+import { getRandom } from '@add/one/utils';
 
 export default class ApplicationController extends Controller {
-    // get random(): string {
-    //     return String(getRandom());
-    // }
+    get random(): string {
+        return String(getRandom());
+    }
 }
