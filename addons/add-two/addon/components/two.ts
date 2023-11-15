6dedbe43523dc0ab4e92c8a74188ece0be9a7bce
Mark Haugen <mhaugen@alarm.com>
2023-11-15 08:39:06 -0500

    Remove libraries

--- addons/add-two/addon/components/two.ts
+++ addons/add-two/addon/components/two.ts
@@ -1,9 +1,7 @@
 import Component from '@glimmer/component';
 import { getRandom } from '@add/one/utils';
-import { inject as service } from '@ember/service';
 
 import type { OneComponentSignature } from '@add/one/components/one';
-import type TestService from '@adc/common/test';
 
 interface TwoComponentSignature {
     Element: OneComponentSignature['Element'];
@@ -13,8 +11,6 @@ interface TwoComponentSignature {
 }
 
 export default class TwoComponent extends Component<TwoComponentSignature> {
-    @service declare responsive: TestService;
-
     get random(): string {
         return String(getRandom());
     }
--- libraries/common/types/test.d.ts
+++
@@ -1,7 +0,0 @@
-import type Service from '@ember/service';
-
-declare module '@adc/common/test' {
-    export default class TestService extends Service {
-        foo: boolean;
-    }
-}
