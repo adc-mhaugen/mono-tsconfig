8c4874d6ec23ff940b61a77699d8313ff53cc24a
Mark Haugen <mhaugen@alarm.com>
2023-12-12 14:36:20 -0500

    Verify in app imports work

--- apps/app-one/app/controllers/application.ts
+++ apps/app-one/app/controllers/application.ts
@@ -1,8 +1,13 @@
 import Controller from '@ember/controller';
 import { getRandom } from '@add/one/utils';
+import { testLocal } from 'app-one/utils/foo';
 
 export default class ApplicationController extends Controller {
     get random(): string {
         return String(getRandom());
+    }
+
+    get local(): ReturnType<typeof testLocal> {
+        return testLocal();
     }
 }
